8a09c2129bb5d055bdbf5eb9b342887ac5dd067a
al-dhani <Alif>
2025-07-09 10:06:19 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,181 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/html.png";
+import Tools3 from "/assets/tools/laravel.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/canva.png";
+import Tools10 from "/assets/tools/css.png";
+import Tools11 from "/assets/tools/blender.png";
+import Tools12 from "/assets/tools/phprad.png";
+import Tools13 from "/assets/tools/figma.png";
+import Tools14 from "/assets/tools/php.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "HTML",
+    ket: "Language",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Laravel",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1100",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Blender",
+    ket: "3D Graphics Software",
+    dad: "1200",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "PhpRad",
+    ket: "PHP Code Generator",
+    dad: "1300",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1400",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1500",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.webp";
+import Proyek2 from "/assets/proyek/proyek2.webp";
+import Proyek3 from "/assets/proyek/proyek3.webp";
+import Proyek4 from "/assets/proyek/proyek4.webp";
+import Proyek5 from "/assets/proyek/proyek5.webp";
+import Proyek6 from "/assets/proyek/proyek6.webp";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Website Belajar Bahasa Jepang",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["HTML", "CSS", "Javascript", "AOS"],
+    dad: "200",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "UI/UX Design",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["HTML", "CSS", "Javascript", "AOS", "Swiper", "Lightbox Gallery"],
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "CRUD Daftar Tugas",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["Vite", "ReactJS", "TailwindCSS", "AOS"],
+    dad: "400",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Website Course",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["Vite", "ReactJS", "Bootstrap", "AOS"],
+    dad: "500",
+  },
+  {
+    id: 5,
+    gambar: Proyek5,
+    nama: "Web Portfolio",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
+    dad: "600",
+  },
+  {
+    id: 6,
+    gambar: Proyek6,
+    nama: "Company Profile 2.0",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["NextJS", "TailwindCSS", "Framermotion"],
+    dad: "700",
+  },
+];
